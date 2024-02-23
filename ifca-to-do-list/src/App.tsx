a0553516a78f534e627738a1b9cc4c8698af203b
judyn97 <jalaludin5011@gmail.com>
2024-02-23 23:30:46 +0800

    Initial project setup
    - Create basic layout for to do list app
    - Set port to port 3333

---
+++ ifca-to-do-list/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>To Do List</h1>
+      <input type='text' placeholder='Add item to the list..'></input>
+      <button> Add </button>
+    </>
+  )
+}
+
+export default App
