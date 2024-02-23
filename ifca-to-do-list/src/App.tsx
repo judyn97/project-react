9b6d123b0949b449e61c0b8c76ddbe85b0964740
judyn97 <jalaludin5011@gmail.com>
2024-02-24 01:21:42 +0800

    Update App.tsx
    - Implement basic to do list functions (add data to list)

--- ifca-to-do-list/src/App.tsx
+++ ifca-to-do-list/src/App.tsx
@@ -1,14 +1,42 @@
+import { useState } from 'react'
 import './App.css'
 
-function App() {
+interface list {
+  id: number;
+  text: string;
+  completed: boolean;
+}
 
+function App() {
+  const [item, setItem] = useState<list[]>([]);
+  const [input, setInput] = useState("");
+
+  {/*Add new item to array when button is clicked*/}
+  const handleClick = () =>{
+    const newItem: list = {id: Date.now(), text:input, completed: false};
+    setItem([...item,newItem]);
+  }
   return (
     <>
-      <h1>To Do List</h1>
-      <input type='text' placeholder='Add item to the list..'></input>
-      <button> Add </button>
+      <>
+        <h1>To Do List</h1>
+        <input className="input-item" type='text' placeholder='Add item to the list..' onChange={(e) => setInput(e.currentTarget.value)}></input>
+        <button className="add-button" onClick={handleClick}> Add </button>
+        <ul>
+          {item.map((todo) => {
+            return(<li key={todo.id}>{todo.text}</li>)
+          })}
+        </ul>
+    </>
     </>
   )
 }
 
 export default App
+
+{/*Remaining implementation:
+1. Add delete todo list or mark as done or both
+2. Make cannot display same item twice ?
+3. Display total list at bottom ?
+4. Style to make it responsive
+*/}
